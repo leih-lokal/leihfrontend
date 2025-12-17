dfbb6cbb66747ef4dc883314f1f0d4bbaa07f9a4
Ruby Morgan Voigt <ruby@rmv.fyi>
2025-12-17 14:58:25 +0100

    yeah

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitc31b1e4fbcb516efa62c0066499090d5
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitc31b1e4fbcb516efa62c0066499090d5', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc31b1e4fbcb516efa62c0066499090d5', 'loadClassLoader'));
